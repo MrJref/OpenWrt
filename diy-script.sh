d4248dbc3b7336dcde6406dec70f9643029f4e5d
MrJref <51068441+MrJref@users.noreply.github.com>
2024-05-15 17:27:55 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -45,10 +45,10 @@ git_sparse_clone https://github.com/sbwml/luci-app-mosdns mosdns
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
 # 科学上网插件
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # Themes
